c06059e86e6413dc21c464c67fd96b473b18701e
orou500 <87994914+orou500@users.noreply.github.com>
2022-05-09 17:31:41 +0300

    Working Chat 1.1

--- server.js
+++ server.js
@@ -83,21 +83,65 @@ app.use((error, req, res, next) => {
 const server = http.createServer(app);
 io = require('socket.io')(server);
 
+let users = []
+
+// const messages = {
+//     general: [],
+//     programmers: [],
+//     support: [],
+//     menu: [],
+//     cloud: [],
+//     accountancy: [],
+// }
 
 io.on('connection', (socket) => {
-    const users = {}
-
-    socket.on('user-name', (name) => {
-        users[socket.id] = name
-        io.emit('connection', name + ' connected')
+    socket.on('join server', (username) => {
+        const user = {
+            username,
+            id: socket.id,
+        }
+        users.push(user)
+        io.emit("new user", users)
     })
+
+    socket.on('chat message', (message, userId) => {
+        let fromUserName = users.find(u => u.id === socket.id);
+        fromUserName = fromUserName.username
+        socket.to(userId).emit('new message', {message: message, from: fromUserName, fromSocket: socket.id})
+    })
+
+    // socket.on('join room', (roomName, cb) => {
+    //     socket.join(roomName)
+    //     cb(messages[roomName])
+    // })
+
+    // socket.on('send message', ({content, to, sender, chatName, isChannel}) => {
+    //     if(isChannel){
+    //         const payload = {
+    //             content,
+    //             chatName,
+    //             sender,
+    //         }
+    //         socket.to(to).emit('new message', payload)
+    //     } else {
+    //         const payload = {
+    //             content,
+    //             chatName: sender,
+    //             sender,
+    //         }
+    //         socket.to(to).emit('new message', payload)
+    //     }
+    //     if(messages[chatName]) {
+    //         messages[chatName].push({
+    //             sender,
+    //             content,
+    //         })
+    //     }
+    // })
 
     socket.on('disconnect', () => {
-        console.log(socket.id +' disconnected')
-    })
-
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', {message: msg, name: users[socket.id]})
+        users = users.filter(u => u.id !== socket.id)
+        io.emit('new user', users)
     })
 
 })
